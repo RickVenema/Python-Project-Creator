c9ab1a7da16a31a373b6955fb01eee3ac37c682c
RickVenema <31951183+RickVenema@users.noreply.github.com>
2019-06-04 22:48:54 +0200

    added first argument with argparser

--- Create/create.py
+++ Create/create.py
@@ -7,4 +7,15 @@ import argparse
 
 class Create:
     def __init__(self):
-        pass
+        self.arguments = self.argparser_create()
+
+    @staticmethod
+    def argparser_create():
+        parser = argparse.ArgumentParser(description="Tool for creating new python projects")
+        parser.add_argument("--git", "-g", default=True, help="Initialize with git repository")
+        args = parser.parse_args()
+        return args
+
+
+if __name__ == '__main__':
+    create = Create()
