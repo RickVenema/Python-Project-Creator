abce84cf8e28fd6c819f0fea25e5b7ffa7f01bc2
RickVenema <31951183+RickVenema@users.noreply.github.com>
2019-06-05 13:21:28 +0200

    added create folder function and argument

--- Create/create.py
+++ Create/create.py
@@ -3,18 +3,39 @@
 """
 
 import argparse
+import os
 
 
 class Create:
     def __init__(self):
+        self.path = os.getcwd()
         self.arguments = self.argparser_create()
+        self.run()
 
     @staticmethod
     def argparser_create():
         parser = argparse.ArgumentParser(description="Tool for creating new python projects")
-        parser.add_argument("--git", "-g", default=True, help="Initialize with git repository")
+        parser.add_argument("--folder_name", "-f", help="The folder name of the project", dest="folder_name")
+        parser.add_argument("--git", "-g",  action='store_true',
+                            help="Initialize with git repository", dest="git")
         args = parser.parse_args()
         return args
+
+    def create_directory(self, name):
+        os.mkdir(self.path + "/" + name)
+
+    def run(self):
+        try:
+            if self.arguments.folder_name:
+                self.create_directory(self.arguments.folder_name)
+            else:
+                raise NotADirectoryError("Folder not specified")
+        except NotADirectoryError:
+            print("Folder not specified")
+            print("use --help (or -h) for more information about the arguments")
+            exit()
+
+        print("jumped")
 
 
 if __name__ == '__main__':
